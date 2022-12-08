12a2b277374b8c5b53d92c4e2e795d8ee99777c9
Nator Junior <51802728+natorjunior@users.noreply.github.com>
2022-12-08 17:31:06 -0300

    Update app.py

--- aula-03/exemplo04/app.py
+++ aula-03/exemplo04/app.py
@@ -24,30 +24,5 @@ contatos = [{
 def hello_world():
   return render_template('index.html',contatos=contatos)
 
-#http://127.0.0.1:5000/salvar_contato/nator%2Fnator%40email.com%2F22222/familia%2Ctrabalho
-@app.route('/salvar_contato',methods=['POST',])
-def salvar_contato():
-  #print((tags.split(',')))
-  contatos.append(
-    {
-        "nome": request.form['nome'],
-        "email": request.form['email'],
-        "celular": request.form['celular'],
-        "tags":request.form['tags'],
-        'links_img_tag': ['familia']#[link_tags[i] for i in tags.split(',')]
-    }
-  )
-  return redirect('/')
-#http://127.0.0.1:5000/deletar_contato/nator%40email.com
-@app.route('/deletar_contato',methods=['POST',])
-def deletar_contato():
-  for i in range(len(contatos)):
-    #print(contatos[i])
-    if contatos[i]['email'] == request.form['email']:
-        contatos.pop(i)
-        break
-    print()
-  return redirect('/')
-
 if __name__ == "__main__":
     app.run(debug=True)
